4771b7a2f4e410706a29e0d3a4f1d3a9d8982510
Szymon P <szymon_jozef@proton.me>
2025-03-18 19:45:59 +0100

    Update .local/bin/system_setup.sh

--- private_dot_local/bin/executable_system_setup.sh
+++ private_dot_local/bin/executable_system_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-sudo pacman -Syu fish neovim uwsm python3 util-linux libnewt wofi kitty yazi playerctl tldr flatpak zen-browser-avx2-bin wl-clipboard hyprland hyprpaper hypridle hyprlock xdg-desktop-portal-hyprland xdg-desktop-portal-gtk scx-scheds pipewire wireplumber pavucontrol man-db fastfetch btop pipewire-audio pipewire-alsa pipewire-pulse mako libnotify feh slurp grim swappy cliphist file nerd-fonts ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick bat eza npm bc vesktop mangohud goverlay steam gimp spotify-launcher obs-studio gamescope gamemode bluez bluez-utils blueman heroic-games-launcher chezmoi github-cli openssh socat otf-font-awesome greetd-regreet papirus-icon-theme hicolor-icon-theme noto-fonts-emoji batsignal waybar --needed --noconfirm
+echo "Packags..."
+sudo pacman -Syu fish neovim uwsm python3 util-linux libnewt wofi kitty yazi playerctl tldr flatpak zen-browser-bin wl-clipboard hyprland hyprpaper hypridle hyprlock xdg-desktop-portal-hyprland xdg-desktop-portal-gtk scx-scheds pipewire wireplumber pavucontrol man-db fastfetch btop pipewire-audio pipewire-alsa pipewire-pulse mako libnotify feh slurp grim swappy cliphist file nerd-fonts ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick bat eza npm bc vesktop mangohud goverlay steam gimp spotify-launcher obs-studio gamescope gamemode bluez bluez-utils blueman heroic-games-launcher chezmoi github-cli openssh socat otf-font-awesome greetd-regreet papirus-icon-theme hicolor-icon-theme noto-fonts-emoji batsignal waybar --needed --noconfirm
 
 if ! groups "$USER" | grep -qw "gamemode"; then
     sudo usermod -aG gamemode "$USER"
@@ -27,8 +28,14 @@ else
 	yay -S hyprsunset hyprpolkitagent --needed
 fi
 
+echo "flatpak..."
+
 flatpak update --noninteractive
 flatpak install --noninteractive net.rpcs3.RPCS3
 
+echo "Services..."
+
 systemctl --user enable hypridle hyprpolkitagent pipewire-pulse hypridle waybar
 sudo systemctl enable bluetooth greetd
+
+echo "All done"
