8d2ce79c67a107fcf7f56bdfb775b55574a01d88
Szymon P <szymon_jozef@proton.me>
2025-06-11 01:18:06 +0200

    Update .config/hypr/hyprland/binds/open_apps.conf Update .config/hypr/hyprland/rules/browser.conf Update .config/hypr/hyprland/rules/feh.conf Update .config/hypr/hyprland/rules/gimp.conf Update .config/hypr/hyprland/rules/heroic.conf Update .config/hypr/hyprland/rules/lutris.conf Update .config/hypr/hyprland/rules/misc.conf Update .config/hypr/hyprland/rules/obs.conf Update .config/hypr/hyprland/rules/pavucontrol.conf Update .config/hypr/hyprland/rules/rpcs3.conf Update .config/hypr/hyprland/rules/signal.conf Update .config/hypr/hyprland/rules/spotify.conf Update .config/hypr/hyprland/rules/steam.conf Update .config/hypr/hyprland/rules/swappy.conf Update .config/hypr/hyprland/rules/vesktop.conf Update .local/bin/start_coding.sh

--- private_dot_local/bin/executable_start_coding.sh
+++ private_dot_local/bin/executable_start_coding.sh
@@ -5,7 +5,7 @@ dir=$(find ~/Dokumenty/kodowanie/ -maxdepth 2 -type d ! -path '.*' | fzf)
 if [[ -n $dir ]]; then
     echo "You choose $dir"
     hyprctl dispatch exec [workspace 5] kitty yazi $dir
-    hyprctl dispatch exec [workspace 5] kitty $dir
+    hyprctl dispatch exec [workspace 5] kitty yazi $dir
     hyprctl dispatch exec [workspace 5] kitty $dir
 else
     echo "No reasonable directory selected, bye bye :P"
