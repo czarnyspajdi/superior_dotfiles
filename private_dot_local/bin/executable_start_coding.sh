b0e27fea52e9216c1a58ffaaf56d22af8540e889
Szymon P <szymon_jozef@proton.me>
2025-06-10 21:55:49 +0200

    Add .local/bin/start_coding.sh

---
+++ private_dot_local/bin/executable_start_coding.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+dir=$(find ~/Dokumenty/kodowanie/ -maxdepth 2 -type d ! -path '.*' | wofi -dmenu)
+
+if [[ -n $dir ]]; then
+    echo "You choose $dir"
+    hyprctl dispatch exec [workspace 5] kitty $dir
+    hyprctl dispatch exec [workspace 5] kitty $dir
+    hyprctl dispatch exec [workspace 5] kitty yazi $dir
+else
+    echo "No reasonable directory selected, bye bye :P"
+fi
