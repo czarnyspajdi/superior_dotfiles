addffc2d9440cde9afa8b2257183d35967c84de7
Szymon P <szymon_jozef@proton.me>
2025-09-29 11:50:23 +0200

    Update .config/hypr/hyprsunset.conf Update .local/bin/system_setup.sh

--- private_dot_local/bin/executable_system_setup.sh
+++ private_dot_local/bin/executable_system_setup.sh
@@ -1,14 +1,129 @@
 #!/bin/bash
-set -e
-echo "Packags..."
-sudo pacman -Syu fish neovim uwsm python3 util-linux libnewt wofi kitty yazi playerctl tldr flatpak zen-browser-bin wl-clipboard hyprland hyprpaper hypridle hyprlock xdg-desktop-portal-hyprland xdg-desktop-portal-gtk scx-scheds pipewire wireplumber pavucontrol man-db fastfetch btop pipewire-audio pipewire-alsa pipewire-pulse mako libnotify feh slurp grim swappy cliphist file nerd-fonts ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick bat eza npm bc vesktop mangohud goverlay steam gimp spotify-launcher obs-studio gamescope gamemode bluez bluez-utils blueman heroic-games-launcher chezmoi github-cli openssh socat otf-font-awesome greetd-regreet papirus-icon-theme hicolor-icon-theme noto-fonts-emoji batsignal waybar --needed --noconfirm
+
+# colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BOLD='\033[1m'
+RESET='\033[0m'
+
+# functions
+function install_yay() {
+    echo "Yay is not installed, installing..."
+    sudo pacman -S --needed git base-devel
+    tmpdir="$(mktemp -d)"
+    git clone https://aur.archlinux.org/yay.git "$tmpdir"
+    cd "$tmpdir"
+    makepkg -si
+    cd -
+    rm -rf "$tmpdir"
+}
+# pkgs lists
+HYPR_PKGS="
+hyprcursor
+hyprgraphics
+hypridle
+hyprland
+hyprland-qt-support
+hyprland-qtutils
+hyprlang
+hyprlock
+hyprpaper
+hyprpolkitagent
+hyprsunset
+hyprutils
+hyprwayland-scanner
+xdg-desktop-portal-hyprland
+xdg-desktop-portal-gtk
+mako
+libnotify
+"
+
+SOUND_PKGS="
+pipewire
+wireplumber
+pipewire-audio
+pipewire-alsa
+pipewire-pulse
+pavucontrol
+"
+UTIL_PKGS="
+fish
+neovim
+uwsm
+python3
+util-linux
+libnewt
+wofi
+kitty
+yazi
+playerctl
+flatpak
+wl-clipboard
+scx-scheds
+man-db
+fastfetch
+btop
+file
+nerd-fonts
+ffmpeg
+7zip
+jq
+poppler
+fd
+ripgrep
+fzf
+zoxide
+imagemagick
+bat
+eza
+npm
+bc
+chezmoi
+github-cli
+openssh
+socat
+otf-font-awesome
+papirus-icon-theme
+hicolor-icon-theme
+noto-fonts-emoji
+cliphist
+"
+
+GUI_PKGS="
+zen-browser-bin
+vesktop
+mangohud
+goverlay
+steam
+gimp
+gamescope
+gamemode
+bluez
+bluez-utils
+blueman
+heroic-games-launcher
+feh
+slurp
+grim
+"
+
+# --- Drukowanie ---
+PRINT_PKGS="
+cups
+cups-browsed
+"
+
+PKGS=( $HYPR_PKGS $SOUND_PKGS $UTIL_PKGS $GUI_PKGS $PRINT_PKGS )
+
+echo "Downloading packages..."
+sudo pacman -Syu --needed "${PKGS[@]}"
 
 if ! groups "$USER" | grep -qw "gamemode"; then
     sudo usermod -aG gamemode "$USER"
 fi
 
 if ! echo $PATH | grep -q "$HOME/.local/bin"; then
-    fish_add_path "$HOME/.local/bin"
+    fish -c "fish_add_path \"$HOME/.local/bin\""
 fi
 
 if ! echo $SHELL | grep -qw "/usr/bin/fish"; then
@@ -16,26 +131,19 @@ if ! echo $SHELL | grep -qw "/usr/bin/fish"; then
 fi
 
 if ! pacman -Q yay &>/dev/null; then
-	echo "Yay is not installed, installing..."
-	sudo pacman -S --needed git base-devel
-	git clone https://aur.archlinux.org/yay.git $HOME/yay
-	cd $HOME/yay
-	makepkg -si
-	cd ..
-	rm yay -rf
+    install_yay
 else
 	echo "Yay is installed..."
-	yay -S hyprsunset hyprpolkitagent --needed
 fi
 
-echo "flatpak..."
+echo "Flatpak install..."
 
 flatpak update --noninteractive
-flatpak install --noninteractive net.rpcs3.RPCS3
+flatpak install --noninteractive com.github.tchx84.Flatseal com.spotify.Client
 
-echo "Services..."
+echo "Services setup..."
 
-systemctl --user enable hypridle hyprpolkitagent pipewire-pulse hypridle waybar hyprpaper
-sudo systemctl enable bluetooth greetd
+systemctl --user enable hypridle hyprpolkitagent pipewire-pulse
+sudo systemctl enable bluetooth cups
 
-echo "All done"
+echo -e "${BOLD}${GREEN}All done!${RESET}"
