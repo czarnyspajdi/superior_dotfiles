cb0a44d1a795b7b441478796a30f5edbc4466a7d
Szymon P <szymon_jozef@proton.me>
2025-11-30 11:15:22 +0100

    Update .config/fish/functions/update.fish

--- private_dot_config/private_fish/functions/executable_update.fish
+++ private_dot_config/private_fish/functions/executable_update.fish
@@ -1,4 +1,12 @@
-function update --description 'alias update=sudo pacman -Syu && flatpak update'
-    yay || sudo pacman -Syu && sudo pacman -Rns (pacman -Qtdq) ; flatpak update --noninteractive $argv
+function update --description "update system"
+    if not yay
+        sudo pacman -Syu
+    end
 
+    set orphans (pacman -Qtdq)
+    if test (count $orphans) -gt 0
+        sudo pacman -Rns $orphans
+    end
+
+    flatpak update --noninteractive $argv
 end
